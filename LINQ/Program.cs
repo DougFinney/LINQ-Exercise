58a3432c7d39797d360774dc8e251b73db94533f
Doug Finney <dugfiny@gmail.com>
2023-01-18 13:41:03 -0600

    Order My Games

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Order My Board Games:");
+            var boardGamesList = new List<string>()
+            {
+                "Monopoly",
+                "Checkers",
+                "Candy Land",
+                "Clue",
+                "Life"
+            };
+
+            //var gamesListOrdered = boardGamesList.OrderBy(name => name.Length);
+
+            //foreach (var game in gamesListOrdered)
+            //{
+            //    Console.WriteLine(game);
+            //}
+            boardGamesList
+                    .OrderBy(name => name.Length)
+                    .ToList()
+                    .ForEach(Console.WriteLine);
         }
     }
 }
